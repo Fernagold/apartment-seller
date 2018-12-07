5b4adb74fa0abcc09d321d190f740f951eb19908
Fernagold <fernagold@gmail.com>
2018-12-07 12:57:24 +0200

    all done!

--- main.js
+++ main.js
@@ -14,7 +14,11 @@ $("button").on("click", function () {
 const renderApts = function (apartments) {
     $("#results").empty()
     console.log(apartments) //array of apartments to render
-    //Your code goes here.
+    const source = $('#result-template').html();
+    const template = Handlebars.compile(source);
+    const newHTML = template({apartments});
+    $("#results").append(newHTML)
+
 }
 
 renderApts(apartments) //renders apartments when page loads
